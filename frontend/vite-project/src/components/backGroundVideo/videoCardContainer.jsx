2543c52b04f90e2d95beee588ad4ee6386d25995
MiguelRioss <92408701+MiguelRioss@users.noreply.github.com>
2025-06-12 05:56:46 +0100

    //Testing autoplay

--- frontend/vite-project/src/components/backGroundVideo/videoCardContainer.jsx
+++ frontend/vite-project/src/components/backGroundVideo/videoCardContainer.jsx
@@ -1,16 +1,22 @@
 import IntroCardComponent from '../IntroCardComponent';
-const src = 'https://www.youtube.com/watch?v=5DD5lNwc7ho';
 const localSrc = '../../res/FREE_LISBON_VIDEO.mp4';
+
 function VideoCard() {
   return (
-    <div className="relative w-full h-screen overflow-hidden ">
-      {/* IntroCardComponent overlays the video without padding */}
-      <div className="absolute inset-0 z-10 mt-10 ">
+    <div className="relative w-full h-screen overflow-hidden">
+      <div className="absolute inset-0 z-10 mt-10">
         <IntroCardComponent />
       </div>
 
-      {/* Background video */}
-      <video className="w-full h-full object-cover " autoPlay loop muted src={localSrc} />
+      <video
+        className="w-full h-full object-cover"
+        autoPlay
+        loop
+        muted
+        playsInline
+        preload="auto"
+        src={localSrc}
+      />
     </div>
   );
 }
